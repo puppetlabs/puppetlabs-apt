ece3d744ffed2c25e2a4acee12d26ab1b5a2ebf7
crayfishx <craig@craigdunn.org>
2012-07-16 15:31:15 -0700

    Fixed handling of apt update refreshonly setting.
    * Parameterized the apt::update class to allow the base class to pass in the value for refreshonly based on the setting of always_apt_update

--- manifests/update.pp
+++ manifests/update.pp
@@ -1,9 +1,9 @@
-class apt::update {
+class apt::update ( $refreshonly = true )  {
   include apt::params
 
   exec { 'apt_update':
     command     => "${apt::params::provider} update",
     logoutput   => 'on_failure',
-    refreshonly => true,
+    refreshonly => $refreshonly,
   }
 }
