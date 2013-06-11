26c4558df1f64bd126515cacec8873273201340c
Nic von Waltsleben <nic@cloudseed.co.za>
2013-06-11 11:27:14 +0200

    Added dependency for packages

--- manifests/update.pp
+++ manifests/update.pp
@@ -6,4 +6,6 @@ class apt::update {
     logoutput   => 'on_failure',
     refreshonly => true,
   }
+
+  Exec['apt_update'] -> Package <| title != "python-software-properties" and title != "software-properties-common" |>
 }
