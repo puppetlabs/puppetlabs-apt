9656924fb3ac08169d99d415a91385b84ba8481c
Juan Treminio <jtreminio@gmail.com>
2013-10-07 16:55:25 -0700

    Revert "This switches us to doing a `test -s` instead of checking for the"
    Reverting change as .list files are being created with no content.
    
    This reverts commit 2f9c16a2736e1de4eda3250984e2b1125efd849e.

--- spec/system/apt_ppa_spec.rb
+++ spec/system/apt_ppa_spec.rb
@@ -5,11 +5,10 @@ describe 'apt::ppa' do
   context 'reset' do
     it 'removes ppa' do
       shell('rm /etc/apt/sources.list.d/drizzle-developers-ppa*')
-      shell('rm /etc/apt/sources.list.d/raravena80-collectd5-*')
     end
   end
 
-  context 'adding a ppa that doesnt exist' do
+  context 'apt::ppa' do
     it 'should work with no errors' do
       pp = <<-EOS
       include '::apt'
@@ -30,29 +29,9 @@ describe 'apt::ppa' do
     end
   end
 
-  context 'readding a removed ppa.' do
-    it 'setup' do
-      shell('add-apt-repository -y ppa:raravena80/collectd5')
-      # This leaves a blank file
-      shell('add-apt-repository --remove ppa:raravena80/collectd5')
-    end
-
-    it 'should readd it successfully' do
-      pp = <<-EOS
-      include '::apt'
-      apt::ppa { 'ppa:raravena80/collectd5': }
-      EOS
-
-      puppet_apply(pp) do |r|
-        r.exit_code.should_not == 1
-      end
-    end
-  end
-
   context 'reset' do
-    it 'removes added ppas' do
+    it 'removes ppa' do
       shell('rm /etc/apt/sources.list.d/drizzle-developers-ppa*')
-      shell('rm /etc/apt/sources.list.d/raravena80-collectd5-*')
     end
   end
 
