ff85e9bea59e5e1c967b61f25b64bdacad567fec
Teran McKinney <teran@thousandeyes.com>
2014-11-07 23:07:58 +0000

    MODULES-1476: Fix fact setting failures if apt-check not present
    Also makes it work in the first place. apt-check writes to stderr, not
    stdout. We need to process stderr. Should handle both, however.

--- lib/facter/apt_updates.rb
+++ lib/facter/apt_updates.rb
@@ -2,7 +2,7 @@ apt_package_updates = nil
 Facter.add("apt_has_updates") do
   confine :osfamily => 'Debian'
   if File.executable?("/usr/lib/update-notifier/apt-check")
-    apt_package_updates = Facter::Util::Resolution.exec('/usr/lib/update-notifier/apt-check 2>/dev/null').split(';')
+    apt_package_updates = Facter::Util::Resolution.exec('/usr/lib/update-notifier/apt-check 2>&1').split(';')
   end
 
   setcode do
@@ -10,10 +10,13 @@ Facter.add("apt_has_updates") do
   end
 end
 
+packages = nil
 Facter.add("apt_package_updates") do
   confine :apt_has_updates => true
   setcode do
-    packages = Facter::Util::Resolution.exec('/usr/lib/update-notifier/apt-check -p 2>/dev/null').split("\n")
+  if File.executable?("/usr/lib/update-notifier/apt-check")
+    packages = Facter::Util::Resolution.exec('/usr/lib/update-notifier/apt-check -p 2>&1').split("\n")
+  end
     if Facter.version < '2.0.0'
       packages.join(',')
     else
@@ -22,16 +25,20 @@ Facter.add("apt_package_updates") do
   end
 end
 
-Facter.add("apt_updates") do
-  confine :apt_has_updates => true
-  setcode do
-    Integer(apt_package_updates[0])
+if apt_updates != nil
+  Facter.add("apt_updates") do
+    confine :apt_has_updates => true
+    setcode do
+      Integer(apt_package_updates[0])
+    end
   end
 end
 
-Facter.add("apt_security_updates") do
-  confine :apt_has_updates => true
-  setcode do
-    Integer(apt_package_updates[1])
+if apt_has_updates != nil
+  Facter.add("apt_security_updates") do
+    confine :apt_has_updates => true
+    setcode do
+      Integer(apt_package_updates[1])
+    end
   end
 end
