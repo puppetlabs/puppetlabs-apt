1df885fe67bad241973d9defb7db25c0ff0eecc7
Teran McKinney <teran@thousandeyes.com>
2014-11-11 21:19:58 +0000

    Fixes

--- lib/facter/apt_updates.rb
+++ lib/facter/apt_updates.rb
@@ -1,22 +1,23 @@
-apt_package_updates = nil
 Facter.add("apt_has_updates") do
   confine :osfamily => 'Debian'
   if File.executable?("/usr/lib/update-notifier/apt-check")
     apt_package_updates = Facter::Util::Resolution.exec('/usr/lib/update-notifier/apt-check 2>&1').split(';')
+  else
+    apt_package_updates = ['31337', '31337']
   end
-
   setcode do
-    apt_package_updates != ['0', '0'] unless apt_package_updates.nil?
+    apt_package_updates
   end
 end
 
-packages = nil
 Facter.add("apt_package_updates") do
   confine :apt_has_updates => true
-  setcode do
   if File.executable?("/usr/lib/update-notifier/apt-check")
     packages = Facter::Util::Resolution.exec('/usr/lib/update-notifier/apt-check -p 2>&1').split("\n")
+  else
+    packages = ['update-notifier-common']
   end
+  setcode do
     if Facter.version < '2.0.0'
       packages.join(',')
     else
@@ -25,20 +26,16 @@ Facter.add("apt_package_updates") do
   end
 end
 
-if apt_updates != nil
-  Facter.add("apt_updates") do
-    confine :apt_has_updates => true
-    setcode do
-      Integer(apt_package_updates[0])
-    end
+Facter.add("apt_updates") do
+  confine :apt_has_updates => true
+  setcode do
+    Integer(apt_package_updates[0])
   end
 end
 
-if apt_has_updates != nil
-  Facter.add("apt_security_updates") do
-    confine :apt_has_updates => true
-    setcode do
-      Integer(apt_package_updates[1])
-    end
+Facter.add("apt_security_updates") do
+  confine :apt_has_updates => true
+  setcode do
+    Integer(apt_package_updates[1])
   end
 end
