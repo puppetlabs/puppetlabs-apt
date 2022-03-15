evolvingweb/puppet-apt

commit 7e31732abd277c852564c10011099bdf6967dc4f

manifests: use modern os facts

manifests/source.pp
@@ -73,10 +73,10 @@ define apt::source(
   $_before = Apt::Setting["list-${title}"]
 
   if !$release {
-    if $facts['lsbdistcodename'] {
-      $_release = $facts['lsbdistcodename']
+    if $facts['os']['distro']['codename'] {
+      $_release = $facts['os']['distro']['codename']
     } else {
-      fail(translate('lsbdistcodename fact not available: release parameter required'))
+      fail(translate('os.distro.codename fact not available: release parameter required'))
     }
   } else {
     $_release = $release
@@ -94,7 +94,7 @@ define apt::source(
     }
     # Newer oses, do not need the package for HTTPS transport.
     $_transport_https_releases = [ 'wheezy', 'jessie', 'stretch', 'trusty', 'xenial' ]
-    if ($facts['lsbdistcodename'] in $_transport_https_releases) and $_location =~ /(?i:^https:\/\/)/ {
+    if ($facts['os']['distro']['codename'] in $_transport_https_releases) and $_location =~ /(?i:^https:\/\/)/ {
       ensure_packages('apt-transport-https')
     }
   } else {

examples/source.pp
@@ -2,7 +2,7 @@
 class { 'apt': }
 
 # Install the puppetlabs apt source
-# Release is automatically obtained from lsbdistcodename fact if available.
+# Release is automatically obtained from facts.
 apt::source { 'puppetlabs':
   location => 'http://apt.puppetlabs.com',
   repos    => 'main',

locales/ja/puppetlabs-apt.po
@@ -69,8 +69,8 @@ msgid "parameters release and origin are mutually exclusive"
 msgstr "パラメータreleaseとoriginは相互に排他的です。"
 
 #. ./manifests/ppa.pp:30 ./manifests/source.pp:79
-msgid "lsbdistcodename fact not available: release parameter required"
-msgstr "lsbdistcodenameのfactが使用できません。releaseパラメータが必要です。"
+msgid "os.distro.codename fact not available: release parameter required"
+msgstr "os.distro.codenameのfactが使用できません。releaseパラメータが必要です。"
 
 #. ./manifests/ppa.pp:34
 msgid "apt::ppa is not currently supported on Debian."

locales/puppetlabs-apt.pot
@@ -92,7 +92,7 @@ msgid "parameters release and origin are mutually exclusive"
 msgstr ""
 
 #. ./manifests/ppa.pp:30 ./manifests/source.pp:79
-msgid "lsbdistcodename fact not available: release parameter required"
+msgid "os.distro.codename fact not available: release parameter required"
 msgstr ""
 
 #. ./manifests/ppa.pp:34

manifests/backports.pp
@@ -21,7 +21,7 @@
 #
 # @param release
 #   Specifies a distribution of the Apt repository containing the backports to manage. Used in populating the `source.list` configuration file.
-#   Default: on Debian and Ubuntu, '${lsbdistcodename}-backports'. We recommend keeping this default, except on other operating
+#   Default: on Debian and Ubuntu, `${facts['os']['distro']['codename']}-backports`. We recommend keeping this default, except on other operating
 #   systems.
 #
 # @param repos
@@ -70,7 +70,7 @@ class apt::backports (
   if $key {
     $_key = $key
   }
-  if (!($facts['lsbdistid'] == 'Debian' or $facts['lsbdistid'] == 'Ubuntu')) {
+  if (!($facts['os']['name'] == 'Debian' or $facts['os']['name'] == 'Ubuntu')) {
     unless $location and $release and $repos and $key {
       fail(translate('If not on Debian or Ubuntu, you must explicitly pass location, release, repos, and key'))
     }
@@ -79,7 +79,7 @@ class apt::backports (
     $_location = $::apt::backports['location']
   }
   unless $release {
-    $_release = "${facts['lsbdistcodename']}-backports"
+    $_release = "${facts['os']['distro']['codename']}-backports"
   }
   unless $repos {
     $_repos = $::apt::backports['repos']

manifests/init.pp
@@ -157,7 +157,7 @@ class apt (
   Boolean $sources_list_force   = $apt::params::sources_list_force,
 ) inherits apt::params {
 
-  if $facts['osfamily'] != 'Debian' {
+  if $facts['os']['family'] != 'Debian' {
     fail(translate('This module only works on Debian or derivatives like Ubuntu'))
   }
 

manifests/params.pp
@@ -1,10 +1,10 @@
 # @summary Provides defaults for the Apt module parameters.
-# 
+#
 # @api private
 #
 class apt::params {
 
-  if $::osfamily != 'Debian' {
+  if $facts['os']['family'] != 'Debian' {
     fail(translate('This module only works on Debian or derivatives like Ubuntu'))
   }
 

manifests/ppa.pp
@@ -10,12 +10,12 @@
 #   Supplies options to be passed to the `add-apt-repository` command. Default: '-y'.
 #
 # @param release
-#   Optional if lsb-release is installed (unless you're using a different release than indicated by lsb-release, e.g., Linux Mint).
 #   Specifies the operating system of your node. Valid options: a string containing a valid LSB distribution codename.
+#   Optional if `puppet facts show os.distro.codename` returns your correct distribution release codename.
 #
 # @param dist
-#   Optional if lsb-release is installed (unless you're using a different release than indicated by lsb-release, e.g., Linux Mint).
 #   Specifies the distribution of your node. Valid options: a string containing a valid distribution codename.
+#   Optional if `puppet facts show os.name` returns your correct distribution name.
 #
 # @param package_name
 #   Names the package that provides the `apt-add-repository` command. Default: 'software-properties-common'.
@@ -26,20 +26,20 @@
 define apt::ppa(
   String $ensure                 = 'present',
   Optional[String] $options      = $::apt::ppa_options,
-  Optional[String] $release      = $facts['lsbdistcodename'],
-  Optional[String] $dist         = $facts['lsbdistid'],
+  Optional[String] $release      = $facts['os']['distro']['codename'],
+  Optional[String] $dist         = $facts['os']['name'],
   Optional[String] $package_name = $::apt::ppa_package,
   Boolean $package_manage        = false,
 ) {
   unless $release {
-    fail(translate('lsbdistcodename fact not available: release parameter required'))
+    fail(translate('os.distro.codename fact not available: release parameter required'))
   }
 
   if $dist == 'Debian' {
     fail(translate('apt::ppa is not currently supported on Debian.'))
   }
 
-  if versioncmp($facts['lsbdistrelease'], '14.10') >= 0 {
+  if versioncmp($facts['os']['release']['full'], '14.10') >= 0 {
     $distid = downcase($dist)
     $dash_filename = regsubst($name, '^ppa:([^/]+)/(.+)$', "\\1-${distid}-\\2")
     $underscore_filename = regsubst($name, '^ppa:([^/]+)/(.+)$', "\\1_${distid}_\\2")
@@ -55,7 +55,7 @@ define apt::ppa(
 
   $sources_list_d_filename  = "${dash_filename_no_specialchars}-${release}.list"
 
-  if versioncmp($facts['lsbdistrelease'], '15.10') >= 0 {
+  if versioncmp($facts['os']['release']['full'], '15.10') >= 0 {
     $trusted_gpg_d_filename = "${underscore_filename_no_specialchars}.gpg"
   } else {
     $trusted_gpg_d_filename = "${dash_filename_no_specialchars}.gpg"

puppetlabs/puppetlabs-apt

commit 72fddbdb11c9ea7518564635e5cc5aaaaa3f3427

Properly fail if the codename fact is unavailable
On Facter 3 the lsb-release package is needed for the os.distro fact to
be present. If it's not, $facts['os']['distro']['codename'] is
essentially undef['codename'] which then fails to compile.

Evaluation Error: Error while evaluating a Resource Statement, Evaluation Error: Operator '[]' is not applicable to an Undef Value.

The fact() function from stdlib doesn't have this problem and properly
returns undef on missing facts. Prior to
7e31732abd277c852564c10011099bdf6967dc4f this wasn't a problem because
it used $facts['lsbdistcodename'] which also returned undef.

Fixes: 7e31732abd277c852564c10011099bdf6967dc4f

manifests/source.pp
@@ -79,7 +79,7 @@ define apt::source(
   $_before = Apt::Setting["list-${title}"]
 
   if !$release {
-    if $facts['os']['distro']['codename'] {
+    if fact('os.distro.codename') {
       $_release = $facts['os']['distro']['codename']
     } else {
       fail('os.distro.codename fact not available: release parameter required')