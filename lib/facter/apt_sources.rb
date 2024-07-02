7e162424533373616fec58318e1f02695a61843e
Bruno Léon <bruno.leon@unyonsys.com>
2024-07-02 13:30:16 +0200

    Add deb822 format ppa support on Ubuntu 24.04
    New repo are added as .sources files.
    Without this update, add-apt-repository keeps being
    called for ppa repo on each Puppet run because if does not look
    for .sources files

--- lib/facter/apt_sources.rb
+++ lib/facter/apt_sources.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
-# This fact lists the .list filenames that are used by apt.
+# This fact lists the .list and .sources filenames that are used by apt.
 Facter.add(:apt_sources) do
   confine osfamily: 'Debian'
   setcode do
     sources = ['sources.list']
     Dir.glob('/etc/apt/sources.list.d/*.list').each do |file|
+      sources.push(File.basename(file))
+    end
+    Dir.glob('/etc/apt/sources.list.d/*.sources').each do |file|
       sources.push(File.basename(file))
     end
     sources
