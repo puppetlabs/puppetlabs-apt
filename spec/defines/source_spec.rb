puppetlabs/puppetlabs-apt

commit 9876c31aff7e738c9d533c556192591f991aa4af

Add support for deb822 APT sources (#1167)
* Initial testing for deb822 APT sources

* Added defined type for generating deb822 apt sources

Should be roughly drop-in alternative to the existing apt::source type
Does not currently support inline ascii gpg key

* Add puppet strings documentation

* Replace example with generic content

* Initial testing of apt::source with deb822 support

* Fix setting_type and signed_by

Use 'sources' instead of 'source' as the setting_type parsed to
apt::setting

Fix the data type of apt::source::signed_by

* Fix source_deb822.epp template

Correctly handle newline/whitespace trimming for `signed_by` parameter.

* Remove link from components to repos

* Add logic for deb822 sources

* Update documentation for deb822 sources

* Update source_format parameter

Match the possible values to the file suffix of the created source
files.

* Add forward-compatibility with deb822

Allow array values for certain parameters to allow easy switching between .list and .sources formats.

* Add backward-compatibility for deb822 sources

Convert string values to arrays where possible and warn the user.

* Fix logic

* Revert "Add forward-compatibility with deb822"

This reverts commit 9d88e9392b816e6996998370ecc29a7f1a3dd9bb.

* Fix deb822 backward compatibility

Correctly compare data type when generating deb822 sources
Remove unused class parameters and descriptions

* Update puppet-strings and logic

Remove references to unused deb822 parameters
Update parameter descriptions
Update deb822 example
Update warnings for $pin and $key usage with deb822. Currently
unsupported
Don't fail if $location is missing unless $ensure is 'present'

* Remove unused spec test

* Add unit testing for deb822 sources

* Update README.md with deb822 sources example

* Fix unit test syntax

* Re-apply commit 2faa817dcb05821bd9c27bbe8bf1955e6b2442f2

eliminate params.pp and create_resources()

manifests/source.pp
@@ -21,8 +21,26 @@
 #     },
 #   }
 #
+# @example Install the puppetlabs apt source (deb822 format)
+#   apt::source { 'puppetlabs':
+#     source_format => 'sources'
+#     location      => ['http://apt.puppetlabs.com'],
+#     repos         => ['puppet8'],
+#     keyring       => '/etc/apt/keyrings/puppetlabs.gpg',
+#   }
+#
+# @param source_format
+#   The file format to use for the apt source. See https://wiki.debian.org/SourcesList
+#
 # @param location
-#   Required, unless ensure is set to 'absent'. Specifies an Apt repository.
+#   Required, unless ensure is set to 'absent'. Specifies an Apt repository. Valid options: a string containing a repository URL.
+#   DEB822: Supports an array of URL values
+#
+# @param types
+#   DEB822: The package types this source manages.
+#
+# @param enabled
+#   DEB822: Enable or Disable the APT source.
 #
 # @param comment
 #   Supplies a comment for adding to the Apt source file.
@@ -32,9 +50,11 @@
 #
 # @param release
 #   Specifies a distribution of the Apt repository.
+#   DEB822: Supports an array of values
 #
 # @param repos
 #   Specifies a component of the Apt repository.
+#   DEB822: Supports an array of values
 #
 # @param include
 #   Configures include options. Valid options: a hash of available keys.
@@ -66,6 +86,7 @@
 #   Tells Apt to only download information for specified architectures. Valid options: a string containing one or more architecture names,
 #   separated by commas (e.g., 'i386' or 'i386,alpha,powerpc').
 #   (if unspecified, Apt downloads information for all architectures defined in the Apt::Architectures option)
+#   DEB822: Supports an array of values
 #
 # @param allow_unsigned
 #   Specifies whether to authenticate packages from this release, even if the Release file is not signed or the signature can't be checked.
@@ -80,177 +101,269 @@
 #   Specifies whether to check if the package release date is valid.
 #
 define apt::source (
-  Optional[String[1]] $location = undef,
+  Enum['list', 'sources'] $source_format = 'list',
+  Array[Enum['deb','deb-src'], 1, 2] $types = ['deb'],
+  Optional[Variant[String[1], Array[String[1]]]] $location = undef,
   String[1] $comment = $name,
+  Boolean $enabled = true, # deb822
   Enum['present', 'absent'] $ensure = present,
-  Optional[String[0]] $release = undef,
-  String[1] $repos = 'main',
+  Optional[Variant[String[0], Array[String[0]]]] $release = undef,
+  Variant[String[1], Array[String[1]]] $repos = 'main',
   Hash $include = {},
   Optional[Variant[String[1], Hash]] $key = undef,
   Optional[Stdlib::AbsolutePath] $keyring = undef,
   Optional[Variant[Hash, Integer, String[1]]] $pin = undef,
-  Optional[String[1]] $architecture = undef,
-  Boolean $allow_unsigned = false,
-  Boolean $allow_insecure = false,
+  Optional[Variant[String[1], Array[String[1]]]] $architecture = undef,
+  Optional[Boolean] $allow_unsigned = undef,
+  Optional[Boolean] $allow_insecure = undef,
+  Optional[Boolean] $check_valid_until = undef,
   Boolean $notify_update = true,
-  Boolean $check_valid_until = true,
 ) {
   include apt
 
   $_before = Apt::Setting["list-${title}"]
 
-  if !$release {
-    if fact('os.distro.codename') {
-      $_release = fact('os.distro.codename')
-    } else {
-      fail('os.distro.codename fact not available: release parameter required')
-    }
-  } else {
-    $_release = $release
-  }
+  case $source_format {
+    'list': {
+      $_file_suffix = $source_format
 
-  if $release =~ Pattern[/\/$/] {
-    $_components = $_release
-  } else {
-    $_components = "${_release} ${repos}"
-  }
-
-  if $ensure == 'present' {
-    if ! $location {
-      fail('cannot create a source entry without specifying a location')
-    }
-    elsif ($apt::proxy['https_acng']) and ($location =~ /(?i:^https:\/\/)/) {
-      $_location = regsubst($location, 'https://','http://HTTPS///')
-    }
-    else {
-      $_location = $location
-    }
-  } else {
-    $_location = undef
-  }
-
-  $includes = $apt::include_defaults + $include
-
-  if $keyring {
-    if $key {
-      fail('parameters key and keyring are mutually exclusive')
-    } else {
-      $_list_keyring = $keyring
-    }
-  } elsif $key {
-    if $key =~ Hash {
-      unless $key['name'] or $key['id'] {
-        fail('key hash must contain a key name (for apt::keyring) or an id (for apt::key)')
-      }
-      if $key['id'] {
-        # defaults like keyserver are only relevant to apt::key
-        $_key = $apt::source_key_defaults + $key
+      if !$release {
+        if fact('os.distro.codename') {
+          $_release = fact('os.distro.codename')
+        } else {
+          fail('os.distro.codename fact not available: release parameter required')
+        }
       } else {
-        $_key = $key
+        $_release = $release
       }
-    } else {
-      $_key = { 'id' => assert_type(String[1], $key) }
-    }
-    if $_key['ensure'] {
-      $_key_ensure = $_key['ensure']
-    } else {
-      $_key_ensure = $ensure
-    }
 
-    # Old keyserver keys handled by apt-key
-    if $_key =~ Hash and $_key['id'] {
-      # We do not want to remove keys when the source is absent.
+      if $release =~ Pattern[/\/$/] {
+        $_components = $_release
+      } else {
+        $_components = "${_release} ${repos}"
+      }
+
       if $ensure == 'present' {
-        apt::key { "Add key: ${$_key['id']} from Apt::Source ${title}":
-          ensure   => $_key_ensure,
-          id       => $_key['id'],
-          server   => $_key['server'],
-          content  => $_key['content'],
-          source   => $_key['source'],
-          options  => $_key['options'],
-          weak_ssl => $_key['weak_ssl'],
-          before   => $_before,
+        if ! $location {
+          fail('cannot create a source entry without specifying a location')
+        }
+        elsif ($apt::proxy['https_acng']) and ($location =~ /(?i:^https:\/\/)/) {
+          $_location = regsubst($location, 'https://','http://HTTPS///')
+        }
+        else {
+          $_location = $location
+        }
+      } else {
+        $_location = undef
+      }
+
+      $includes = $apt::include_defaults + $include
+
+      if $keyring {
+        if $key {
+          fail('parameters key and keyring are mutually exclusive')
+        } else {
+          $_list_keyring = $keyring
+        }
+      } elsif $key {
+        if $key =~ Hash {
+          unless $key['name'] or $key['id'] {
+            fail('key hash must contain a key name (for apt::keyring) or an id (for apt::key)')
+          }
+          if $key['id'] {
+            # defaults like keyserver are only relevant to apt::key
+            $_key = $apt::source_key_defaults + $key
+          } else {
+            $_key = $key
+          }
+        } else {
+          $_key = { 'id' => assert_type(String[1], $key) }
+        }
+        if $_key['ensure'] {
+          $_key_ensure = $_key['ensure']
+        } else {
+          $_key_ensure = $ensure
+        }
+
+        # Old keyserver keys handled by apt-key
+        if $_key =~ Hash and $_key['id'] {
+          # We do not want to remove keys when the source is absent.
+          if $ensure == 'present' {
+            apt::key { "Add key: ${$_key['id']} from Apt::Source ${title}":
+              ensure   => $_key_ensure,
+              id       => $_key['id'],
+              server   => $_key['server'],
+              content  => $_key['content'],
+              source   => $_key['source'],
+              options  => $_key['options'],
+              weak_ssl => $_key['weak_ssl'],
+              before   => $_before,
+            }
+          }
+          $_list_keyring = undef
+        }
+        # Modern apt keyrings
+        elsif $_key =~ Hash and $_key['name'] {
+          apt::keyring { $_key['name']:
+            ensure   => $_key_ensure,
+            content  => $_key['content'],
+            source   => $_key['source'],
+            dir      => $_key['dir'],
+            filename => $_key['filename'],
+            mode     => $_key['mode'],
+            before   => $_before,
+          }
+
+          $_list_keyring = if $_key['dir'] and $_key['filename'] {
+            "${_key['dir']}${_key['filename']}"
+          } elsif $_key['filename'] {
+            "/etc/apt/keyrings/${_key['filename']}"
+          } elsif $_key['dir'] {
+            "${_key['dir']}${_key['name']}"
+          } else {
+            "/etc/apt/keyrings/${_key['name']}"
+          }
+        }
+      } else {
+        # No `key` nor `keyring` provided
+        $_list_keyring = undef
+      }
+
+      $header = epp('apt/_header.epp')
+
+      if $architecture {
+        $_architecture = regsubst($architecture, '\baarch64\b', 'arm64')
+      } else {
+        $_architecture = undef
+      }
+
+      $source_content = epp('apt/source.list.epp', {
+          'comment'          => $comment,
+          'includes'         => $includes,
+          'options'          => delete_undef_values({
+              'arch'              => $_architecture,
+              'trusted'           => $allow_unsigned ? { true => 'yes', false => undef, default => undef },
+              'allow-insecure'    => $allow_insecure ? { true => 'yes', false => undef, default => undef },
+              'signed-by'         => $_list_keyring,
+              'check-valid-until' => $check_valid_until? { true => undef, false => 'false', default => undef },
+            },
+          ),
+          'location'         => $_location,
+          'components'       => $_components,
+        }
+      )
+
+      if $pin {
+        if $pin =~ Hash {
+          $_pin = $pin + { 'ensure' => $ensure, 'before' => $_before }
+        } elsif ($pin =~ Numeric or $pin =~ String) {
+          $url_split = split($location, '[:\/]+')
+          $host      = $url_split[1]
+          $_pin = {
+            'ensure'   => $ensure,
+            'priority' => $pin,
+            'before'   => $_before,
+            'origin'   => $host,
+          }
+        } else {
+          fail('Received invalid value for pin parameter')
+        }
+
+        apt::pin { $name:
+          * => $_pin,
         }
       }
-      $_list_keyring = undef
     }
-    # Modern apt keyrings
-    elsif $_key =~ Hash and $_key['name'] {
-      apt::keyring { $_key['name']:
-        ensure   => $_key_ensure,
-        content  => $_key['content'],
-        source   => $_key['source'],
-        dir      => $_key['dir'],
-        filename => $_key['filename'],
-        mode     => $_key['mode'],
-        before   => $_before,
+    'sources': {
+      $_file_suffix = $source_format
+
+      if $pin {
+        warning("'pin' parameter is not supported with deb822 format.")
+      }
+      if $key {
+        warning("'key' parameter is not supported with deb822 format.")
+      }
+      if $ensure == 'present' {
+        if ! $location {
+          fail('cannot create a source entry without specifying a location')
+        }
+      }
+      if (type($location, 'generalized') !~ Type[Array]) {
+        warning('For deb822 sources, location must be specified as an array.')
+        $_location = [$location]
+      }
+      else {
+        $_location = $location
       }
 
-      $_list_keyring = if $_key['dir'] and $_key['filename'] {
-        "${_key['dir']}${_key['filename']}"
-      } elsif $_key['filename'] {
-        "/etc/apt/keyrings/${_key['filename']}"
-      } elsif $_key['dir'] {
-        "${_key['dir']}${_key['name']}"
+      if !$release {
+        if fact('os.distro.codename') {
+          $_release = [fact('os.distro.codename')]
+        } else {
+          fail('os.distro.codename fact not available: release parameter required')
+        }
       } else {
-        "/etc/apt/keyrings/${_key['name']}"
+        if (type($release, 'generalized') !~ Type[Array]) {
+          warning("For deb822 sources, 'release' must be specified as an array. Converting to array.")
+          $_release = [$release]
+        } else {
+          $_release = $release
+        }
+      }
+
+      if (type($repos, 'generalized') !~ Type[Array]) {
+        warning("For deb822 sources, 'repos' must be specified as an array. Converting to array.")
+        $_repos = split($repos, /\s+/)
+      } else {
+        $_repos = $repos
+      }
+
+      if $architecture != undef {
+        if (type($architecture, 'generalized') !~ Type[Array]) {
+          warning("For deb822 sources, 'architecture' must be specified as an array. Converting to array.")
+          $_architecture = split($architecture, '[,]')
+        }
+        else {
+          $_architecture = $architecture
+        }
+      } else {
+        $_architecture = $architecture
+      }
+      case $ensure {
+        'present': {
+          $header = epp('apt/_header.epp')
+          $source_content = epp('apt/source_deb822.epp', delete_undef_values({
+                'uris'              => $_location,
+                'suites'            => $_release,
+                'components'        => $_repos,
+                'types'             => $types,
+                'comment'           => $comment,
+                'enabled'           => $enabled ? { true => 'yes', false => 'no' },
+                'architectures'     => $_architecture,
+                'allow_insecure'    => $allow_insecure ? { true => 'yes', false => 'no', default => undef },
+                'repo_trusted'      => $allow_unsigned ? { true => 'yes', false => 'no', default => undef },
+                'check_valid_until' => $check_valid_until ? { true => 'yes', false => 'no', default => undef },
+                'signed_by'         => $keyring,
+              }
+            )
+          )
+        }
+        'absent': {
+          $header = undef
+          $source_content = undef
+        }
+        default: {
+          fail('Unexpected value for $ensure parameter.')
+        }
       }
     }
-  } else {
-    # No `key` nor `keyring` provided
-    $_list_keyring = undef
+    default: {
+      fail("Unexpected APT source format: ${source_format}")
+    }
   }
-
-  $header = epp('apt/_header.epp')
-
-  if $architecture {
-    $_architecture = regsubst($architecture, '\baarch64\b', 'arm64')
-  } else {
-    $_architecture = undef
-  }
-
-  $sourcelist = epp('apt/source.list.epp',
-    {
-      'comment'          => $comment,
-      'includes'         => $includes,
-      'options'          => delete_undef_values(
-        {
-          'arch'              => $_architecture,
-          'trusted'           => $allow_unsigned ? { true => 'yes', false => undef },
-          'allow-insecure'    => $allow_insecure ? { true => 'yes', false => undef },
-          'signed-by'         => $_list_keyring,
-          'check-valid-until' => $check_valid_until? { true => undef, false => 'false' },
-        },
-      ),
-      'location'         => $_location,
-      'components'       => $_components,
-    },
-  )
-
-  apt::setting { "list-${name}":
+  apt::setting { "${_file_suffix}-${name}":
     ensure        => $ensure,
-    content       => "${header}${sourcelist}",
+    content       => "${header}${source_content}",
     notify_update => $notify_update,
-  }
-
-  if $pin {
-    if $pin =~ Hash {
-      $_pin = $pin + { 'ensure' => $ensure, 'before' => $_before }
-    } elsif ($pin =~ Numeric or $pin =~ String) {
-      $url_split = split($location, '[:\/]+')
-      $host      = $url_split[1]
-      $_pin = {
-        'ensure'   => $ensure,
-        'priority' => $pin,
-        'before'   => $_before,
-        'origin'   => $host,
-      }
-    } else {
-      fail('Received invalid value for pin parameter')
-    }
-
-    apt::pin { $name:
-      * => $_pin,
-    }
   }
 }

spec/defines/source_spec.rb
@@ -416,4 +416,69 @@ describe 'apt::source' do
       it { is_expected.to contain_apt__setting("list-#{title}").with_notify_update(false) }
     end
   end
+
+  describe 'deb822 sources' do
+    let :params do
+      {
+        source_format: 'sources',
+      }
+    end
+
+    context 'basic deb822 source' do
+      let :params do
+        super().merge(
+          {
+            location: ['http://debian.mirror.iweb.ca/debian/'],
+            repos: ['main', 'contrib', 'non-free']
+          },
+        )
+      end
+
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_notify_update(true) }
+    end
+
+    context 'complex deb822 source' do
+      let :params do
+        super().merge(
+          {
+            types: ['deb', 'deb-src'],
+            location: ['http://fr.debian.org/debian', 'http://de.debian.org/debian'],
+            release: ['stable', 'stable-updates', 'stable-backports'],
+            repos: ['main', 'contrib', 'non-free'],
+            architecture: ['amd64', 'i386'],
+            allow_unsigned: true,
+            notify_update: false
+          },
+        )
+      end
+
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_notify_update(false) }
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_content(%r{Enabled: yes}) }
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_content(%r{Types: deb deb-src}) }
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_content(%r{URIs: http://fr.debian.org/debian http://de.debian.org/debian}) }
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_content(%r{Suites: stable stable-updates stable-backports}) }
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_content(%r{Components: main contrib non-free}) }
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_content(%r{Architectures: amd64 i386}) }
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_content(%r{Trusted: yes}) }
+    end
+
+    context '.list backwards compatibility' do
+      let :params do
+        super().merge(
+          {
+            location: 'http://debian.mirror.iweb.ca/debian/',
+            release: 'unstable',
+            repos: 'main contrib non-free',
+            key: {
+              id: 'A1BD8E9D78F7FE5C3E65D8AF8B48AD6246925553',
+              server: 'keyserver.ubuntu.com',
+            },
+            pin: '-10'
+          },
+        )
+      end
+
+      it { is_expected.to contain_apt__setting("sources-#{title}").with_notify_update(true) }
+    end
+  end
 end

manifests/init.pp
@@ -213,6 +213,10 @@ class apt (
       'path' => $sources_list_d,
       'ext'  => '.list',
     },
+    'sources' => {
+      'path' => $sources_list_d,
+      'ext'  => '.sources',
+    },
   },
   Boolean $sources_list_force = false,
   Hash $source_key_defaults = {

manifests/setting.pp
@@ -40,8 +40,8 @@ define apt::setting (
   $setting_type = $title_array[0]
   $base_name = join(delete_at($title_array, 0), '-')
 
-  assert_type(Pattern[/\Aconf\z/, /\Apref\z/, /\Alist\z/], $setting_type) |$a, $b| {
-    fail("apt::setting resource name/title must start with either 'conf-', 'pref-' or 'list-'")
+  assert_type(Pattern[/\Aconf\z/, /\Apref\z/, /\Alist\z/, /\Asources\z/], $setting_type) |$a, $b| {
+    fail("apt::setting resource name/title must start with either 'conf-', 'pref-', 'list-', or 'sources-'")
   }
 
   if $priority !~ Integer {
@@ -51,7 +51,7 @@ define apt::setting (
     }
   }
 
-  if ($setting_type == 'list') or ($setting_type == 'pref') {
+  if $setting_type in ['list', 'pref', 'sources'] {
     $_priority = ''
   } else {
     $_priority = $priority

templates/source_deb822.epp
@@ -1,0 +1,36 @@
+<% | String $comment,
+  Enum['yes','no'] $enabled,
+  Array[String] $types,
+  Array[String] $uris,
+  Array[String] $suites,
+  Array[String] $components,
+  Optional[Array] $architectures = undef,
+  Optional[Enum['yes','no']] $allow_insecure = undef,
+  Optional[Enum['yes','no']] $repo_trusted = undef,
+  Optional[Enum['yes','no']] $check_valid_until = undef,
+  Optional[Variant[Stdlib::AbsolutePath,Array[String]]] $signed_by = undef,
+| -%>
+# <%= $comment %>
+Enabled: <%= $enabled %>
+Types: <% $types.each |String $type| { -%> <%= $type %> <% } %>
+URIs: <% $uris.each | String $uri | { -%> <%= $uri %> <% } %>
+Suites: <% $suites.each | String $suite | { -%> <%= $suite %> <% } %>
+Components: <% $components.each | String $component | { -%> <%= $component %> <% } %>
+<% if $architectures { -%>
+Architectures:<% $architectures.each | String $arch | { %> <%= $arch %><% } %>
+<%- } -%>
+<% if $allow_insecure { -%>
+Allow-Insecure: <%= $allow_insecure %>
+<% } -%>
+<% if $repo_trusted { -%>
+Trusted: <%= $repo_trusted %>
+<% } -%>
+<% if $check_valid_until { -%>
+Check-Valid-Until: <%= $check_valid_until %>
+<% } -%>
+<% if $signed_by { -%>
+Signed-By: <% if type($signed_by) =~ Type[Array] { -%><%- $signed_by.each |String $keyring| { -%><%= $keyring %> <% } -%>
+<%- } -%>
+<%- elsif type($signed_by) =~ Type[String] { -%>
+<%= $signed_by -%>
+<%- }} %>

commit 00c401c4523632369b04078792ff88f0a9c8a9a6

Handle repos passed as an array for list format
In 9876c31aff7e738c9d533c556192591f991aa4af the parameter repos was
changed to no longer allow an empty string. It does allow an array, but
that isn't specifically handled for the list format.

Fixes: 9876c31aff7e ("Add support for deb822 APT sources (#1167)")

manifests/source.pp
@@ -139,6 +139,8 @@ define apt::source (
 
       if $release =~ Pattern[/\/$/] {
         $_components = $_release
+      } elsif $repos =~ Array {
+        $_components = join([$_release] + $repos, ' ')
       } else {
         $_components = "${_release} ${repos}"
       }

spec/defines/source_spec.rb
@@ -36,6 +36,24 @@ describe 'apt::source' do
       it {
         expect(subject).to contain_apt__setting('list-my_source').with(ensure: 'present').without_content(%r{# my_source\ndeb-src hello.there wheezy main\n})
       }
+
+      context 'with repos' do
+        context 'as empty array' do
+          let(:params) { super().merge(repos: []) }
+
+          it {
+            expect(subject).to contain_apt__setting('list-my_source').with(ensure: 'present').without_content(%r{# my_source\ndeb-src hello.there wheezy\n})
+          }
+        end
+
+        context 'as non-empty array' do
+          let(:params) { super().merge(repos: ['main', 'non-free', 'contrib']) }
+
+          it {
+            expect(subject).to contain_apt__setting('list-my_source').with(ensure: 'present').without_content(%r{# my_source\ndeb-src hello.there wheezy main non-free contrib\n})
+          }
+        end
+      end
     end
   end
 